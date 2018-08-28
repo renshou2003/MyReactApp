e901f32eb6d63da93246f42ff6d65cacc21cf8cb
company-computer <31425237+renshou2003@users.noreply.github.com>
2018-08-29 04:13:47 +0800

    表单测试

--- src/components/tests/ReduxHello.js
+++ src/components/tests/ReduxHello.js
@@ -1,7 +1,74 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Form, Icon, Input, Button, Select, Modal } from 'antd';
 
-export default class ReduxHello extends Component {
-  render() {
-    return <div />;
-  }
+const FormItem = Form.Item;
+const Option = Select.Option;
+class ReduxHello extends Component {
+	handleSelectChange = (value) => {
+		console.log(value);
+		this.props.form.setFieldsValue({
+			userName: `Hi, ${value === 'male' ? 'man' : 'lady'}!`
+		});
+	};
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.form.validateFields((err, values) => {
+			console.log(err);
+			if (!err) {
+				Modal.info({title: 'This is an info message',
+        content: 'some messages...some messages...',okText:'知道了'});
+			} else {
+				var arra = [];
+				Object.keys(err).map((key) => 
+        err[key].errors.map((e) => arra.push(e.message))
+				);
+				Modal.error({title: 'This is an error message',
+        content: arra.join(','),okText:'知道了'});
+			}
+		});
+	};
+	render() {
+		const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+		// Only show error after a field is touched.
+		const userNameError = isFieldTouched('userName') && getFieldError('userName');
+		const passwordError = isFieldTouched('password') && getFieldError('password');
+
+		return (
+			<div>
+				<Form onSubmit={this.handleSubmit}>
+					<FormItem validateStatus={userNameError ? 'error' : ''} help={userNameError || ''}>
+						{getFieldDecorator('userName', {
+							rules: [ { required: true, message: 'Please input your username!' } ]
+						})(
+							<Input
+								prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+								placeholder="enter your name"
+							/>
+						)}
+					</FormItem>
+					<FormItem validateStatus={userNameError ? 'error' : ''} help={userNameError || ''}>
+						{getFieldDecorator('like', {
+							rules: [ { required: true, message: 'Please input your username!' } ]
+						})(
+							<Select
+								placeholder="Select a option and change input text above"
+								onChange={this.handleSelectChange}
+							>
+								<Option value="male">male</Option>
+								<Option value="female">female</Option>
+							</Select>
+						)}
+					</FormItem>
+					<FormItem wrapperCol={{ span: 12, offset: 5 }}>
+						<Button type="primary" htmlType="submit">
+							Submit
+						</Button>
+					</FormItem>
+				</Form>
+			</div>
+		);
+	}
 }
+
+export default Form.create()(ReduxHello);
--- src/routes/config.js
+++ src/routes/config.js
@@ -2,7 +2,7 @@ export default {
 	// 功能菜单相关路由
 	menus: [
 		{ key: '/app/dashboard/index', title: '首页', icon: 'mobile', component: 'Dashboard' },
-		{ key: '/app/hello', title: '测试', icon: 'mobile', component: 'ReduxHello' }
+		{ key: '/app/tests/hello', title: '测试', icon: 'mobile', component: 'ReduxHello' }
 	],
 	// 非功能菜单相关路由
 	others: []
